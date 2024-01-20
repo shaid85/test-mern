3cbde46bf2284910411ffb65c4cbe8dbb3900f8d
Shaid Islam <shaid85@gmail.com>
2024-01-20 21:50:37 +0600

    vercel.json client

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'https://test-mern-api-theta.vercel.app'
+      // '/api': 'http://localhost:3000'
     }
   },
   plugins: [react()],
