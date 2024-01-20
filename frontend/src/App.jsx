9c0617e728599610cd59300d4ea2594ed6d7dbf6
Shaid Islam <shaid85@gmail.com>
2024-01-20 20:21:11 +0600

    all file added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios';
+
+
+
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+          .then((response) => {
+            setJokes(response.data)
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+  }, [])
+
+  return (
+    <>
+      <h1>React Frontend</h1>
+      <h2>Jokes: {jokes.length}</h2>
+      {jokes.map((item) => (
+        <div className="joke" key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
