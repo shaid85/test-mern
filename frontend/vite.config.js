8a32dafd31114c65a75ab0a45650257db32a2014
Shaid Islam <shaid85@gmail.com>
2024-01-20 23:09:36 +0600

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         target: 'https://test-mern-api-theta.vercel.app',
         // target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }  
   },
