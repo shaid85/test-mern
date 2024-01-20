d84ed49c20f7124dc62d84298630b1e45f74463f
Shaid Islam <66918424+shaid85@users.noreply.github.com>
2024-01-20 21:06:07 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://test-mern-api-theta.vercel.app/'
+      '/api': 'https://test-mern-api-theta.vercel.app'
     }
   },
   plugins: [react()],
